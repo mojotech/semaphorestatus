753846e53251e5fa199ab6bb8f0fa7fbd5dd49ea
Sam Saccone <samccone@gmail.com>
2013-02-08 15:17:08 -0500

    if their is no commit info do not print branch status

--- check.js
+++ check.js
@@ -27,22 +27,24 @@ function authTokenParams() {
 
 
 function prettyResultView(data) {
-  if (data.result == 'pending') {
-    return String("PENDING" + " " + data.commit.author_name).cyan.inverse + " "+
-            "pushed " + moment(data.commit.timestamp).fromNow().yellow +
-            "\n  "+ data.commit.message.replace("\n", " ") +"\n  " +
-            data.build_url.underline;
-  }
-  else if (data.result == 'failed') {
-    return String("☹" + " " + data.commit.author_name).red.inverse + " " +
+  if (data.commit) {
+    if (data.result == 'pending') {
+      return String("PENDING" + " " + data.commit.author_name).cyan.inverse + " "+
+              "pushed " + moment(data.commit.timestamp).fromNow().yellow +
+              "\n  "+ data.commit.message.replace("\n", " ") +"\n  " +
+              data.build_url.underline;
+    }
+    else if (data.result == 'failed') {
+      return String("☹" + " " + data.commit.author_name).red.inverse + " " +
+              moment(data.finished_at).fromNow().yellow +
+              "\n  "+ data.commit.message.replace("\n", " ") +"\n  " +
+              data.build_url.underline;
+    }
+    return String("☻" + " " + data.commit.author_name).green + " " +
             moment(data.finished_at).fromNow().yellow +
             "\n  "+ data.commit.message.replace("\n", " ") +"\n  " +
-            data.build_url.underline;
+            data.commit.url.underline;
   }
-  return String("☻" + " " + data.commit.author_name).green + " " +
-          moment(data.finished_at).fromNow().yellow +
-          "\n  "+ data.commit.message.replace("\n", " ") +"\n  " +
-          data.commit.url.underline;
 }
 
 function getBranchDetails(hashID) {
